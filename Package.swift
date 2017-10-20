cd46d87434fe5ed72a1c3ddc4a728273f2f23d0e
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-20 17:28:37 +0900

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -6,21 +6,18 @@ import PackageDescription
 let package = Package(
     name: "HDHTTPServer",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "HDHTTPServer",
             targets: ["HDHTTPServer"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+         .package(url: "git@github.com:IBM-Swift/BlueSignals.git", from: "0.9.50"),
+         .package(url: "git@github.com:swift-server/http.git", from: "0.1.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HDHTTPServer",
-            dependencies: []),
+            dependencies: ["Signals", "HTTP"]),
         .testTarget(
             name: "HDHTTPServerTests",
             dependencies: ["HDHTTPServer"]),
